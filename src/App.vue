1511890f84f3562649311aaef5d5e67cfcac4e7a
JeromeLalunio <josejeromelalunio@gmail.com>
2021-07-13 17:36:14 +0800

    replace ts entrypoint with js

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ greeting }}</div>
+  <button v-on:click="alert">click</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      greeting: "hello world",
+    };
+  },
+  methods: {
+    alert() {
+      window.alert("hello world");
+    },
+  },
+});
+</script>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+console.log("it's working!");
+
+createApp(App).mount("#app");
